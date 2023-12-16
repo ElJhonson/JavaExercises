875066276f68723604fab702afa3d86632966349
ElJhonson34 <angel.alvarez20@hotmail.com>
2023-12-16 01:25:09 -0600

    System Class exercises

--- com/system/PropertiesSystem.java
+++ com/system/PropertiesSystem.java
@@ -4,7 +4,7 @@ import java.util.Properties;
 
 public class PropertiesSystem {
     public static void main(String[] args) {
-        //Con esto pudes ver las propiedades de tu sistema, como su nombre lo indica, uno por uno
+        //Con esto pudes ver las propiedades de tu sistema, como su nombre lo indica, uno por uno.
         String username = System.getProperty("user.name");
         System.out.println(username);
 

7b0edd5b3c099539271f3158d81e11578cae0a1e
ElJhonson34 <angel.alvarez20@hotmail.com>
2023-12-22 20:34:25 -0600

    rotate a sorted array

---
+++ src/com/arrays/InsertionSortArray.java
@@ -0,0 +1,28 @@
+package com.arrays;
+
+public class InsertionSortArray {
+    public static void main(String[] args) {
+        String[] names = {"Lalo", "Alita", "Laura", "Luis", "Oscar", "Renata"};
+        int size = names.length;
+        String aux;
+        int pos;
+
+
+        for (int i = 0; i < size; i++) {
+            pos = i;
+            aux = names[i];
+
+            while ((pos > 0) && (names[i - 1].compareTo(aux) > 0)) {
+                names[pos] = names[pos - 1];
+                pos--;
+            }
+            names[pos] = aux;
+        }
+
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+
+}
